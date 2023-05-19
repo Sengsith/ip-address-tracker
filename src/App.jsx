2caf042217e372ec94589165e0662ffcf81d0bc9
Angelo Rosario <angelorosario10@gmail.com>
2023-05-19 13:23:00 -0700

    removed backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 import InputIP from "./components/InputIP";
 import ResultCard from "./components/ResultCard";
 
 const App = () => {
   // Going to create state in here and pass it into InputIP, get it back and pass results into ResultCard
-
-  useEffect(() => {
-    // Get a response from backend
-    const options = {
-      method: "GET",
-      url: "http://localhost:8000/address",
-    };
-
-    axios
-      .request(options)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className="app">
       <h1 className="title">IP Address Tracker</h1>
-      <InputIP />
-      <ResultCard />
+      <InputIP setInputValue={setInputValue} />
+      <ResultCard inputValue={inputValue} />
       <div className="result"></div>
     </div>
   );
--- src/components/InputIP.jsx
+++ src/components/InputIP.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import iconArrow from '../assets/icon-arrow.svg';
+import React from "react";
+import iconArrow from "../assets/icon-arrow.svg";
 
-const InputIP = () => {
+const InputIP = ({ setInputValue }) => {
   return (
     <div className="input-ip__wrapper container">
-      <input className="input-ip" type="text" placeholder="Enter IP address..."></input>
+      <input
+        className="input-ip"
+        type="text"
+        placeholder="Enter IP address..."
+      ></input>
       <button className="arrow-btn">
-        <img className="arrow-icon" src={iconArrow} alt="Arrow right submit button" />
+        <img
+          className="arrow-icon"
+          src={iconArrow}
+          alt="Arrow right submit button"
+        />
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default InputIP;
