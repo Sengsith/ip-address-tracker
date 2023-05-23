ed3540a37652fb695efc89528ce36f140637a3d8
Angelo Rosario <angelorosario10@gmail.com>
2023-05-23 15:22:16 -0700

    Fixed leaflet map, need to add map event

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,21 +1,37 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+import iconLocation from "../assets/icon-location.svg";
 
 const LeafletMap = ({ resultObject }) => {
+  const { location } = resultObject;
+  const customIcon = L.icon({
+    iconUrl: iconLocation,
+    iconSize: [50, 61],
+  });
+
+  // Todo: Move map when user inputs another IP address into user input
+
   return (
-    <div className="map-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          {/* <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup> */}
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer
+      center={
+        location == undefined ? [40.65, -73.94] : [location.lat, location.lng]
+      }
+      zoom={18}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker
+        position={
+          location == undefined ? [40.65, -73.94] : [location.lat, location.lng]
+        }
+        icon={customIcon}
+      ></Marker>
+    </MapContainer>
   );
 };
 
